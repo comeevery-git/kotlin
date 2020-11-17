bbeb843dd12e1be01e32e98beb5d069d09155e15
comeevery-git <comeevery@gmail.com>
2020-11-17 22:18:41 +0900

    day02 - 2

---
+++ kotlin/day02/src/ex12.kt
@@ -0,0 +1,23 @@
+fun main() {
+    var s1:String = "0"
+    var n2:Int = 10
+
+    if(n2 == 10) {
+        s1 = "n2는 10입니다"
+    } else {
+        s1 = "n2는 10이 아닙니다"
+    }
+    println("s1 : $s1")
+
+    val s2:String = if(n2==10) "n2는 10입니다" else "n2는 10이 아닙니다"
+    println("s2 : $s2")
+
+    val s3:String = if(n2 == 10){
+        "n2는 10입니다"
+    } else {
+        "n2는 10이 아닙니다"
+    }
+    println("s3 : $s3")
+
+
+}
